2094af7525cae5d5a327e0c5ae4bbf2ff0ade284
Barbara <varya.sheptunova@gmail.com>
2023-02-19 13:13:16 +0300

    PrintArray method

--- Project/Program.cs
+++ Project/Program.cs
@@ -5,3 +5,11 @@ string[] GetArray()
     string[] result = ReadLine()!.Split(",", StringSplitOptions.RemoveEmptyEntries);
     return result;
 }
+
+void PrintArray(string[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        Write($"{inArray[i]} ");
+    }
+}
