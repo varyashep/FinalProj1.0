9d0f2bd0ec07c980a00a77a95cf66bce67948bca
Barbara <varya.sheptunova@gmail.com>
2023-02-19 13:14:02 +0300

    CheckElements method

--- Project/Program.cs
+++ Project/Program.cs
@@ -6,6 +6,21 @@ string[] GetArray()
     return result;
 }
 
+string[] CheckElements(string[] inArray)
+{
+    string[] newArray = new string[inArray.Length];
+    int number = 0;
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (inArray[i].Length <= 3)
+        {
+            newArray[number] = inArray[i];
+            number++;
+        }
+    }
+    return newArray;
+}
+
 void PrintArray(string[] inArray)
 {
     for (int i = 0; i < inArray.Length; i++)
